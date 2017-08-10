7b1ae1fd0f985dca01e2d86adcd141f7c44311ab
liuhaifeng <1932736402@qq.com>
2017-08-10 15:55:46 +0800

    Initial commit

---
+++ app/src/main/java/com/baway/myapplication/HttpUtil.java
@@ -0,0 +1,29 @@
+package com.baway.myapplication;
+
+/**
+ * Created by Administrator on 2017/8/8.
+ */
+
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+
+/**
+ * Http工具类
+ * 注意声明权限
+ *  <uses-permission android:name="android.permission.ACCESS_NETWORK_STATE"/>
+ *  <uses-permission android:name="android.permission.INTERNET"/>
+ */
+public class HttpUtil {
+
+    //网络请求OKHttp
+    public static void sendOkHttpRequest(String address, Callback callback){
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url(address)
+                .build();
+        client.newCall(request).enqueue(callback);
+    }
+
+
+}
